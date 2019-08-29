0f3a0ea5c0a36b6707032dc414eda45150673db6
omar yousry <omayousry@gmail.com>
2019-08-29 16:17:06 +0200

    adding kryo to tcp xpt

--- AsynchronousSocketServer/src/main/java/xptObj/tcp/Client.java
+++ AsynchronousSocketServer/src/main/java/xptObj/tcp/Client.java
@@ -1,39 +1,53 @@
 package xptObj.tcp;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import objects.Lineitem;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
     private Client(String address, int port) {
         ObjectInputStream in = null;
-        ObjectOutputStream out = null;
         Socket socket = null;
         String line = "";
 
+        Kryo kryo = new Kryo();
+        registerKryoClasses(kryo);
         try {
             socket = new Socket(address, port);
             System.out.println("Connected With Spark Client");
-            in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
 
+            Input input = new Input(socket.getInputStream());
+//            in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+            int count = 0;
+            System.out.println("Waiting");
             long start = System.currentTimeMillis();
             while (line != null) {
                 try {
-                    in.readObject();
-    //                System.out.println(in.readObject());
+                    Lineitem x = kryo.readObject(input, Lineitem.class);
+//                    in.readObject();
+                    System.out.println(x.getL_orderkey());
                 } catch (Exception e) {
                     line = null;
                 }
             }
             long finish = System.currentTimeMillis();
+            System.out.println("Finished an : " + finish + " Secs");
             System.out.println("Finished in : " + ((finish - start)/ 1000) + " Secs");
-            out.close();
             socket.close();
         } catch(IOException i) {
             System.out.println(i);
         }
     }
 
+    private void registerKryoClasses(Kryo kryo) {
+        kryo.register(ArrayList.class);
+        kryo.register(Lineitem.class);
+    }
     public static void main(String[] args) {
         new Client("127.0.0.1", 6000);
     }
--- AsynchronousSocketServer/src/main/java/xptObj/tcp/Server.java
+++ AsynchronousSocketServer/src/main/java/xptObj/tcp/Server.java
@@ -1,5 +1,7 @@
 package xptObj.tcp;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
 import objects.Lineitem;
 
 import java.io.*;
@@ -9,54 +11,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-    private Server(int port) {
+    private Server(int port, String inputPath) {
         DataInputStream rawData;
-        ObjectOutputStream outStream;
-        List<Lineitem> lineitems = new ArrayList<>();
-        try {
-            ServerSocket server = new ServerSocket(port);
+//        ObjectOutputStream outStream;
+        Output outStream;
+
+            List<Lineitem> lineitems = new ArrayList<>();
+
+        Kryo kryo = new Kryo();
+        registerKryoClasses(kryo);
+
+        try (ServerSocket server = new ServerSocket(port)) {
             System.out.println("Spark Server started");
             System.out.println("Waiting for Incorta Client ...");
 
-            Socket socket = server.accept();
-            outStream = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Connected with Incorta Client");
+            try (Socket socket = server.accept()){
+//                outStream = new ObjectOutputStream(socket.getOutputStream());
+                outStream =  new Output(socket.getOutputStream());
+                System.out.println("Connected with Incorta Client");
 
-            rawData = new DataInputStream(new FileInputStream("/home/yousry/dummy_db/tpch/10/tbl/lineitem/lineitem.txt"));
-            String newLine = rawData.readLine();
-            while (newLine != null){
-                String[] items = newLine.split("\\|");
-                lineitems.add(new Lineitem(Long.parseLong(items[0]), Long.parseLong(items[1]), Long.parseLong(items[2]), Long.parseLong(items[3]),
-                        Double.parseDouble(items[4]), Double.parseDouble(items[5]), Double.parseDouble(items[6]), Double.parseDouble(items[7]),
-                        items[8], items[9], items[10], items[11], items[12], items[13], items[14], items[15]));
-                newLine = rawData.readLine();
+                rawData = new DataInputStream(new FileInputStream(inputPath));
+                registerLineItems(lineitems, rawData);
+                Thread.sleep(10000);
+
+                long start = System.currentTimeMillis();
+
+//                kryo.writeObject(outStream, lineitems);
+//                Output finalOutStream = outStream;
+                lineitems.forEach(object -> {
+                    try {
+                        kryo.writeObject(outStream, object);
+//                        outStream.writeObject(object);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+                long finish = System.currentTimeMillis();
+                System.out.println("Started at : " + start + " Secs");
+                System.out.println("Finished in : " + ((finish - start)/ 1000) + " Secs");
+                rawData.close();
+                outStream.close();
             }
-            System.out.println("Finished Serializing Data");
-
-            Thread.sleep(10000);
-            ObjectOutputStream finalOutStream = outStream;
-            long start = System.currentTimeMillis();
-            lineitems.forEach(object -> {
-                try {
-                    finalOutStream.writeObject(object);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            long finish = System.currentTimeMillis();
-            System.out.println("Finished in : " + ((finish - start)/ 1000) + " Secs");
-            rawData.close();
-            outStream.close();
-            socket.close();
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args) {
-        new Server(6000);
+    private void registerLineItems(List<Lineitem> lineItems, DataInputStream rawData) throws IOException {
+        System.out.println("Starting Serializing Data");
+        String newLine = rawData.readLine();
+
+        while (newLine != null){
+            String[] items = newLine.split("\\|");
+
+            lineItems.add(new Lineitem(Long.parseLong(items[0]), Long.parseLong(items[1]), Long.parseLong(items[2]), Long.parseLong(items[3]),
+                    Double.parseDouble(items[4]), Double.parseDouble(items[5]), Double.parseDouble(items[6]), Double.parseDouble(items[7]),
+                    items[8], items[9], items[10], items[11], items[12], items[13], items[14], items[15]));
+            newLine = rawData.readLine();
+        }
+        System.out.println("Finished Serializing Data");
     }
 
-}
+    private void registerKryoClasses(Kryo kryo) {
+        kryo.register(ArrayList.class);
+        kryo.register(Lineitem.class);
+    }
 
+    public static void main(String[] args) {
+        new Server(6000, "/home/yousry/dummy_db/tpch/1/tbl2/lineitem.150k.1");
+    }
+}
